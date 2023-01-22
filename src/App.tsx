99bcad2a05f32335445ca1b187a43482cca389cf
kida-works <kida.works.1234@gmail.com>
2023-01-23 02:44:39 +0900

    todolist Create additional functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import React, { useState } from "react";
+import type { FC } from "react";
 
-function App() {
+// type
+type itemTypes = {
+  title: string;
+  addTime: string;
+  endTime: string;
+};
+
+// コンポーネント
+const item = (title: string, addTime: string, endTime: string) => {
+  return (
+    <li className="list">
+      <span className="listTitle">{title}</span>
+      <span className="listAddtime">{addTime}</span>
+      <span className="listEndTime">{endTime}</span>
+    </li>
+  );
+};
+
+const App = () => {
+  const [list, setList] = useState<itemTypes[]>([]);
+  const [title, setTitle] = useState<string>("");
+
+  const setTime = () => {
+    const today = new Date();
+    const time = `${today.getFullYear()}年 ${
+      today.getMonth() + 1
+    }月 ${today.getDate()}日 ${today.getHours()}時 ${today.getMinutes()}分`;
+    return time;
+  };
+
+  const itemAddFunc = (newTitle: string) => {
+    const addList: itemTypes = {
+      title: newTitle,
+      addTime: setTime(),
+      endTime: "",
+    };
+    setList([...list, addList]);
+    setTitle("")
+  };
+
+  const listsComp = list.map((item) => {
+    return (
+      <li key={item.title}>
+        <span className="listTitle">{item.title}</span>
+        <span className="listAddtime">{item.addTime}</span>
+        <span className="listEndTime">{item.endTime}</span>
+      </li>
+    );
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>to do list</h1>
       </header>
+      <main>
+        <div>
+          <input
+            type="text"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+          />
+          <button onClick={() => itemAddFunc(title)}>追加</button>
+        </div>
+        <ul>{listsComp}</ul>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
